a8cc066ea794f5bf4f124c8476516ac52e05c04a
Anmol Vashistha <anmolvashisthaav@gmail.com>
2019-08-13 09:55:09 +0530

    init

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace GradeBook {
+    class Book {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name) {
+            this.name=name;
+            grades=new List<double>();
+        }
+
+        public void AddGrade(double grade){
+
+        }
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Science");
+            book.AddGrade(89.1);
+            var sum=0.0;
+            List<double> grades=new List<double>() {24.5,67.8};
+            foreach(var grade in grades){
+                sum+=grade;
+            }
+            var average=sum/grades.Count;
+            System.Console.WriteLine($"{average:N3}");
+            //Console.WriteLine($"Hello {args[0]}!");
+        }
+    }
+}
